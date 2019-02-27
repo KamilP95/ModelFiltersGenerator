cec6ea7f3285ca06a724c155593dd735af715ea3
Kamil Partyka <kpartyka@pgs-soft.com>
2019-02-27 11:52:16 +0100

    displaying refactoring action suggestion when caret on class name token

---
+++ ModelFiltersGenerator/GenerateModelFiltersCodeRefactoringProvider.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeRefactorings;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ModelFiltersGenerator
+{
+    [ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = "GenerateModelFiltersCS")]
+    internal class GenerateModelFiltersCodeRefactoringProvider : CodeRefactoringProvider
+    {
+        public override async Task ComputeRefactoringsAsync(CodeRefactoringContext context)
+        {
+            var document = context.Document;
+            var textSpan = context.Span;
+            var cancelationToken = context.CancellationToken;
+
+            var root = await document
+                .GetSyntaxRootAsync(cancelationToken)
+                .ConfigureAwait(false) as CompilationUnitSyntax;
+            var semanticModel = await document.GetSemanticModelAsync(cancelationToken).ConfigureAwait(false);
+
+            if (root == null || semanticModel == null)
+            {
+                return;
+            }
+
+            var token = root.FindToken(textSpan.Start);
+
+            if (!token.IsKind(SyntaxKind.IdentifierToken)
+                || !token.Parent.IsKind(SyntaxKind.ClassDeclaration))
+            {
+                return;
+            }
+
+            var action = CodeAction.Create(
+                "Create filters for model",
+                ct => GenerateModelFiltersAsync(document, root, semanticModel, ct),
+                equivalenceKey: nameof(GenerateModelFiltersCodeRefactoringProvider));
+
+            context.RegisterRefactoring(action);
+        }
+
+        private Task<Document> GenerateModelFiltersAsync(
+            Document document,
+            CompilationUnitSyntax root,
+            SemanticModel semanticModel,
+            CancellationToken cancellationToken)
+        {
+            return Task.FromResult(document);
+        }
+    }
+}
